d7fa737551afb0dadb5fb54bf19c8c0bf0ae4331
macrotek <ganuda1234@gmail.com>
2022-12-26 20:43:42 +0530

    Answer Repository added

---
+++ StackOverflowProject.Repositories/AnswersRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+
+namespace StackOverflowProject.Repositories
+{
+    public interface IAnswersRepository
+    {
+        void InsertAnswer(Answer a);
+        void UpdateAnswer(Answer a);
+        void UpdateAnswerVotesCount(int aid, int uid, int value);
+        void DeleteAnswer(int aid);
+        List<Answer> GetAnswersByQuestionID(int qid);
+        List<Answer> GetAnswersByAnswerID(int AnswerID);
+    }
+    public class AnswersRepository : IAnswersRepository
+    {
+        StackOverflowDatabaseDbContext db;
+        IQuestionsRepository qr;
+        IVotesRepository vr;
+
+        public AnswersRepository()
+        {
+            db = new StackOverflowDatabaseDbContext();
+            qr = new QuestionsRepository();
+            vr = new IVotesRepository();
+        }
+        public void InsertAnswer(Answer a)
+        {
+            db.Answers.Add(a);
+            db.SaveChanges();
+            qr.UpdateQuestionAnswerCount(a.QuestionID, 1);
+        }
+        public void UpdateAnswer(Answer a)
+        {
+            Answer ans = db.Answers.Where(temp => temp.AnswerID == a.AnswerID).FirstOrDefault();
+            if(ans != null)
+            {
+                ans.AnswerText = a.AnswerText;
+                db.SaveChanges();
+            }
+        }
+    }
+}
