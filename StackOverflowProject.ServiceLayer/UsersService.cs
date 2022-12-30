1586d95910d4afd9e567771a2fba71092cf6083b
macrotek <ganuda1234@gmail.com>
2022-12-30 23:24:21 +0530

    Working with Service Layer

---
+++ StackOverflowProject.ServiceLayer/UsersService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+using StackOverflowProject.ViewModels;
+using StackOverflowProject.Repositories;
+using AutoMapper;
+
+namespace StackOverflowProject.ServiceLayer
+{
+    public interface IUsersService
+    {
+        int InsertUser(RegisterViewModel uvm);
+        void UpdateUserDetails(EditUserDetailsViewModel uvm);
+        void UpdateUserPassword(EditUserPasswordViewModel uvm);
+        void DeleteUser(int uid);
+        List<UserViewModel> GetUsers();
+        UserViewModel GetUsersByEmailAndPassword(string Email, string Password);
+        UserViewModel GetUsersByEmail(string Email);
+        UserViewModel GetUsersByUserID(int UserID);
+    }
+    public class UsersService:IUsersService
+    {
+        IUsersRepository ur;
+
+        public UsersService()
+        {
+            ur = new UsersRepository();
+        }
+        public int InsertUser(RegisterViewModel uvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<RegisterViewModel, User>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            User u = mapper.Map<RegisterViewModel, User>(uvm);
+            u.PasswordHash = SHA256HashGenerator.GenerateHash(uvm.Password);
+            ur.InsertUser(u);
+            int uid = ur.GetLatestUserID();
+            return uid;
+        }
+        public void UpdateUserDetails(EditAnswerViewModel uvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<EditUserDetailsViewModel, User>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            User u = mapper.Map<EditUserDetailsViewModel, User>(uvm);
+            ur.UpdateUserDetails(u);
+        }
+    }
+}
