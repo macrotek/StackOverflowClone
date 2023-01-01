9f59f65641e8779e38a99f8504ac9b0f732eceb2
macrotek <ganuda1234@gmail.com>
2023-01-01 17:27:17 +0530

    Question and Category service added

---
+++ StackOverflowProject.ServiceLayer/QuestionService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+using StackOverflowProject.Repositories;
+using StackOverflowProject.ViewModels;
+using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace StackOverflowProject.ServiceLayer
+{
+    public interface IQuestionService
+    {
+        void InsertQuestion(NewQuestionViewModel qum);
+        void UpdateQuestionDetails(EditQuestionViewModel qvm);
+        void UpdateQuestionVotesCount(int qid, int value);
+        void UpdateQuestionAnswersCount(int qid, int value);
+        void UpdateQuestionViewsCount(int qid, int value);
+        void DeleteQuestion(int qid);
+        List<QuestionViewModel> GetQuestions();
+        QuestionViewModel GetQuestionByQuestionID(int QuestionID, int UserID);
+    }
+    public class QuestionService : IQuestionService
+    {
+        IQuestionsRepository qr;
+        public QuestionService()
+        {
+            qr = new QuestionsRepository();
+        }
+        public void InsertQuestion(NewQuestionViewModel qvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<NewQuestionViewModel, Question>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Question q = mapper.Map<NewQuestionViewModel, Question>(qvm);
+            qr.InsertQuestion(q);
+        }
+        public void UpdateQuestionDetails(EditQuestionViewModel qvm)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<EditQuestionViewModel, Question>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            Question q = mapper.Map<EditQuestionViewModel, Question>(qvm);
+            qr.UpdateQuestionDetails(q);
+        }
+        public void UpdateQuestionVotesCount(int qid, int value)
+        {
+            qr.UpdateQuestionVotesCount(qid, value);
+        }
+        public void UpdateQuestionAnswersCount(int qid, int value)
+        {
+            qr.UpdateQuestionAnswerCount(qid, value);
+        }
+        public void UpdateQuestionViewsCount(int qid, int value)
+        {
+            qr.UpdateQuestionViewsCount(qid, value);
+        }
+        public void DeleteQuestion(int qid)
+        {
+            qr.DeleteQuestion(qid);
+        }
+        public List<QuestionViewModel> GetQuestions()
+        {
+            List<Question> q = qr.GetQuestions();
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Question, QuestionViewModel>(); cfg.IgnoreUnmapped(); });
+            IMapper mapper = config.CreateMapper();
+            List<QuestionViewModel> qvm = mapper.Map<List<Question>, List<QuestionViewModel>>(q);
+            return qvm;
+        }
+        public QuestionViewModel GetQuestionByQuestionID(int QuestionID, int UserID=0)
+        {
+            Question q = qr.GetQuestionByQuestionID(QuestionID).FirstOrDefault();
+            QuestionViewModel qvm = null;
+            if(q !=null)
+            {
+                var config = new MapperConfiguration(cfg => { cfg.CreateMap<Question, QuestionViewModel>(); cfg.IgnoreUnmapped(); });
+                IMapper mapper = config.CreateMapper();
+                qvm = mapper.Map<Question, QuestionViewModel>(q);
+
+                foreach(var item in qvm.Answers)
+                {
+                    item.CurrentUserVoteType = 0;
+                    VoteViewModel vote = item.Votes.Where(temp => temp.UserID == UserID).FirstOrDefault();
+                    if(vote != null)
+                    {
+                        item.CurrentUserVoteType = vote.VoteValue;
+                    }
+                }
+            }
+            return qvm;
+        }
+    }
+}
